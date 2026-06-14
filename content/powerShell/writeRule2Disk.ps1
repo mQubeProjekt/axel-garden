2008d55b905e6941901ce5be855d59854d56e740
mQubeProjekt <projekt.mqube@gmail.com>
2026-06-14 16:54:30 +0200

    Add files via upload

---
+++ content/powerShell/writeRule2Disk.ps1
@@ -0,0 +1,11 @@
+﻿. ".\config.ps1"
+. ".\SqlTools.ps1"
+
+$result = Get-RuleSQL -RuleID 99
+
+
+Write-Host "Zeilen gefunden: " $result.Count
+
+$result | Out-GridView -Title "SQL-Tabelle"
+
+Read-Host "Enter drücken"
